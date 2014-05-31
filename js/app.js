68dc30bde7ad786f3d27132db6df8fa8cb56ba96
dwerdo <dwerdo@gmail.com>
2014-05-30 17:38:35 -0700

    madLibs refactor

--- js/app.js
+++ js/app.js
@@ -1,15 +1,34 @@
 angular.module('myApp', [])
+	.controller('FormCtrl', function($scope, $rootScope) {
+		$scope.submit = function() {
+			if($scope.myForm.$valid) {
+				console.log("Valid");
+				$rootScope.results = true;
+			} else {
+				console.log("Invalid");
+				
+			}
+		};
+		$scope.reset = function() {
+			console.log("clicked");
+      		$scope.madLibs = {};
+      		console.log($scope.madLibs);
+      		$scope.myForm.$setPristine();
+      		$scope.submitted = false;
+      		$scope.results = false;
+		}
+	})
 	.controller('MyCtrl', function($scope) {
 		$scope.madLibs = 
 		{
-			male_name: 'male name',
-			job_title: 'job title',
-			tedious_task: 'tedious task',
-			dirty_task: 'dirty task',
-			celebrity: 'celebrity',
-			useless_skill: 'useless skill',
-			obnoxious_celebrity: 'obnoxious celebrity',
-			huge_number: 'huge number',
-			adjective: 'adjective'
+			// male_name: 'male name',
+			// job_title: 'job title',
+			// tedious_task: 'tedious task',
+			// dirty_task: 'dirty task',
+			// celebrity: 'celebrity',
+			// useless_skill: 'useless skill',
+			// obnoxious_celebrity: 'obnoxious celebrity',
+			// huge_number: 'huge number',
+			// adjective: 'adjective'
 		};
 	});
