68bfc3a259c7c51051b451e4a193f15d0c978fea
Ed <ed@edhuang.com>
2014-08-12 21:34:18 +0800

    updated files

--- js/app.js
+++ js/app.js
@@ -1,34 +1,29 @@
+// app.js
+
+//ngAnimate Dependency
 angular.module('myApp', ['ngAnimate'])
-	.controller('FormCtrl', function($scope, $rootScope) {
-		$scope.submit = function() {
-			if($scope.myForm.$valid) {
-				console.log("Valid");
-				$rootScope.results = true;
-			} else {
-				console.log("Invalid");
-				
-			}
-		};
-		$scope.reset = function() {
-			console.log("clicked");
-      		$scope.madLibs = {};
-      		console.log($scope.madLibs);
-      		$scope.myForm.$setPristine();
-      		$scope.submitted = false;
-      		$rootScope.results = false;
+
+//Create an Angular Controller
+.controller('FormCtrl', function( $scope) {
+
+	//Submit Funciton when Form is submitted
+	$scope.submit = function() {
+		$scope.submitted = true;
+		
+		//If the form is valid
+		if($scope.myForm.$valid) {
+			$scope.results = true;
+		} else {
+			console.log("Invalid");
+			
 		}
-	})
-	.controller('MyCtrl', function($scope) {
-		$scope.madLibs = 
-		{
-			// male_name: 'male name',
-			// job_title: 'job title',
-			// tedious_task: 'tedious task',
-			// dirty_task: 'dirty task',
-			// celebrity: 'celebrity',
-			// useless_skill: 'useless skill',
-			// obnoxious_celebrity: 'obnoxious celebrity',
-			// huge_number: 'huge number',
-			// adjective: 'adjective'
-		};
-	});
+	};
+	
+	//Reset Our Variables and Form
+	$scope.reset = function() {
+  		$scope.madLibs = {};
+  		$scope.myForm.$setPristine();
+  		$scope.submitted = false;
+  		$scope.results = false;
+	}
+});
